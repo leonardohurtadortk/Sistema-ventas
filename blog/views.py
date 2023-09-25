6208c5e4987e4d9401aef9b90315b52702276f7b
leonardortk <leotk02@gmail.com>
2023-09-25 23:26:50 +0200

    ddddd

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render
 
 def inicio(request):
     ejemplo = "hola mundo"
-    dof="cccyuyuyudddddd"
+    dof="cccyuyuyuddddddddddssssss"
     return render (request,"inicio.html")
